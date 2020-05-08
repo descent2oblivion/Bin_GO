82896ee22e94d278568506f0958e24206b9ffd48
descent2oblivion <58195806+descent2oblivion@users.noreply.github.com>
2020-05-07 18:36:32 -0700

    create: Nambas

---
+++ app/src/main/java/com/example/bin_go/Nambas.kt
@@ -0,0 +1,32 @@
+class Nambas {
+    var nums : ArrayList<String>? = arrayListOf()
+
+    fun fillML (){
+        for (x in 1..75){
+            if(x in 1..15){
+                nums?.add("B $x")
+            }
+
+            if(x in 16..30){
+                nums?.add("I $x")
+            }
+
+            if(x in 31..45){
+                nums?.add("N $x")
+            }
+
+            if(x in 46..60){
+                nums?.add("G $x")
+            }
+
+            if(x in 61..75){
+                nums?.add("O $x")
+            }
+        }
+    }
+
+    fun inic() {
+        fillML()
+        nums?.shuffle()
+    }
+}
